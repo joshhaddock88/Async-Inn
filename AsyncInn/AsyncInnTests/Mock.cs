5323f605751f162580e8e0fe1667a5c134756e03
Joshua Haddock <joshhaddock88@gmail.com>
2021-08-26 13:30:25 -0700

    Working swagger

---
+++ AsyncInn/AsyncInnTests/Mock.cs
@@ -0,0 +1,21 @@
+﻿using AsyncInn.Data;
+using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncInnTests
+{
+    public abstract class Mock : IDisposable
+    {
+        private readonly SqliteConnection _connections;
+        protected readonly AsyncInnDbContext _db;
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
